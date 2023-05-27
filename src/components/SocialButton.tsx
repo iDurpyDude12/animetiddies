d8ddcc651f63240ff6000ce589297ac9339eab53
durpyneko <89787577+iDurpyDude12@users.noreply.github.com>
2023-05-27 16:11:22 +0200

    yes.

--- src/components/SocialButton.tsx
+++ src/components/SocialButton.tsx
@@ -1,22 +1,20 @@
 import { Button } from "@chakra-ui/react";
- 
-export default function SocialButton( { name, href, icon }) {
-    
+
+export default function SocialButton({ name, href, icon }: any) {
     return (
-       <Button
-        as={"a"}
-        href={href}
-        target={"_blank"}
-        flex={1}
-        width={"400px"}
-        borderRadius="5"
-        textAlign={"center"}
-        padding={3}
-        colorScheme="pink"
-        marginBottom={2}>
-        {icon}
-        &nbsp;{name}
-    </Button>
+        <Button
+            as={"a"}
+            href={href}
+            target={"_blank"}
+            flex={1}
+            width={"400px"}
+            borderRadius="5"
+            textAlign={"center"}
+            padding={3}
+            colorScheme="pink"
+            marginBottom={2}>
+            {icon}
+            &nbsp;{name}
+        </Button>
     );
-    
 }
