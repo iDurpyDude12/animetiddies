737514d84e9947ceb6b2d7407c2690b9c4a78c1c
durpyneko <89787577+iDurpyDude12@users.noreply.github.com>
2023-05-27 16:13:30 +0200

    aaaaaaeeeee

--- src/components/TabHeader.tsx
+++ src/components/TabHeader.tsx
@@ -1,7 +1,7 @@
-export default function TabHeader( { title } ) {
+export default function TabHeader({ title }: any) {
     return (
         <div>
-            <title>{ title }</title>
+            <title>{title}</title>
             <link rel="icon" type="image/png" href="https://cdn.discordapp.com/attachments/1039951514631933992/1083404187989196820/senko_bread.png" />
         </div>
     );
