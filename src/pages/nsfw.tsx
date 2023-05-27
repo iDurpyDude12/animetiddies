55071aafd7447b23f77b913db2c73d8e3d15f1f6
durpyneko <89787577+iDurpyDude12@users.noreply.github.com>
2023-05-27 16:46:47 +0200

    Update nsfw.tsx

--- src/pages/nsfw.tsx
+++ src/pages/nsfw.tsx
@@ -4,16 +4,11 @@ import TabHeader from "src/components/TabHeader";
 import ImagesComponent from "src/components/NSFWImages";
 import { useSession } from "next-auth/react";
 import { Flex, Text } from "@chakra-ui/react";
-import { useRouter } from "next/router";
 
 function NSFW() {
     const { data: session } = useSession();
-    const router = useRouter();
 
     if (session == null) {
-        // redirect to /restricted when NSFW is loaded when not logged in
-        router.push("/restricted");
-
         return (
             <Flex align={"center"}>
                 <Text flex={1} align={"center"} fontSize={"3xl"} marginTop={4}>
