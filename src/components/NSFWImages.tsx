f59d4fc0e32ec1ac966e41bfec7bb0c4bed92b7a
durpyneko <89787577+iDurpyDude12@users.noreply.github.com>
2023-06-03 03:20:05 +0200

    Update NSFWImages.tsx

--- src/components/NSFWImages.tsx
+++ src/components/NSFWImages.tsx
@@ -9,25 +9,6 @@ export default function ImagesComponent() {
 
     let downloadImageUrl;
 
-    const url = downloadImageUrl;
-
-    downloadImage(url);
-    function downloadImage(url: any) {
-        /* fetch(url, {
-            mode: "no-cors"
-        })
-            .then(response => response.blob())
-            .then(blob => {
-                let blobUrl = window.URL.createObjectURL(blob);
-                let a = document.createElement("a");
-                a.download = url.replace(/^.*[\\\/]/, "");
-                a.href = blobUrl;
-                document.body.appendChild(a);
-                a.click();
-                a.remove();
-            }); */
-    }
-
     const getNeko = () => {
         fetch("https://api.waifu.pics/nsfw/neko")
             .then(response => response.json())
@@ -112,7 +93,7 @@ export default function ImagesComponent() {
                             <Box boxSize="sm" paddingLeft={5} paddingTop={5} marginBottom={10}>
                                 <Image id="WaifuImage" maxHeight={400} maxWidth={335} src={imageUrl} alt="Image"></Image>
                             </Box>
-                            <Button id="downloadButton" onClick={downloadImage} colorScheme={"pink"} marginTop={4} width={100}>
+                            <Button id="downloadButton" colorScheme={"pink"} marginTop={4} width={100}>
                                 Download
                             </Button>
                             <Text flex={1} paddingLeft={5} marginTop={4}>
